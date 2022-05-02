436f0540ebe3e595b226b67b820b768cd09fc6ca
naAlmanzor <neilalmanzor28@gmail.com>
2022-05-02 17:39:53 +0800

    added obstacle interactions

--- js/Scenes/Level1Scene.js
+++ js/Scenes/Level1Scene.js
@@ -46,36 +46,39 @@ export class Level1Scene extends Phaser.Scene {
         this.otherworld_details_front = this.map.createLayer('other-world-details front', this.tileset_3, 0, 40);
         this.otherworld_details_back = this.map.createLayer('other world-details back', this.tileset_3, 0, 40);
 
+        //Allows specific layers to be collided
         this.grass_platform.setCollisionByExclusion(-1, true);
         this.brick_platform.setCollisionByExclusion(-1, true);
         this.obstacles.setCollisionByExclusion(-1, true);
         this.trigger.setCollisionByExclusion(-1, true);
         this.hidden_path_front.setCollisionByExclusion(-1, true);
-        this.otherworld_platform.setCollisionByExclusion(-1, true);
+        this.otherworld_platform.setCollisionByExclusion(-1, true); 
 
         // Player and Monster
         this.player = new Player(100, 300, this.physics, this.anims, this.input);
+        this.player.player.invulnerable = false;
         this.player.setWorldCollider(false);
-        // this.player.player.setScale(1.5);
         // this.monster = new Monster(this.physics, this.anims, this.player.player, this.input); 
             
         //Collisions
         this.physics.add.collider(this.player.player, this.grass_platform);
         this.physics.add.collider(this.player.player, this.brick_platform);
-        
-        this.specialCollision = this.physics.add.collider(this.player.player, this.hidden_path_front);
-
-        this.physics.add.collider(this.player.player, this.trigger, this.triggerSet, null, this);
-        this.physics.add.collider(this.player.player, this.obstacles);
         this.physics.add.collider(this.player.player, this.otherworld_platform);
 
+        //Allows the collision to be selected
+        this.specialCollision = this.physics.add.collider(this.player.player, this.hidden_path_front);
+        
+        // Special collision conditions
+        this.physics.add.collider(this.player.player, this.trigger, this.triggerSet, null, this);
+        this.physics.add.collider(this.player.player, this.obstacles, this.hitPlayer, null, this);
+        
         //Camera to follow player
         this.cameras.main
         .setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
         .startFollow(this.player.player);
 
         this.cameras.main.setZoom(2.5);
-        // this.cameras.main.setBackgroundColor('#ffffff')
+        this.cameras.main.setBackgroundColor('#ffffff')
     }
 
     update(){
@@ -88,5 +91,22 @@ export class Level1Scene extends Phaser.Scene {
         player.setPosition(670, 340);
         this.hidden_path_back.destroy();
         this.hidden_path_front.destroy();
+    }
+
+    hitPlayer(player, obstacle){
+
+        player.setVelocityY(-200)
+        
+        if (player.invulnerable == false){
+                player.setTint(0xb025a7);   
+                player.invulnerable = true;
+        }
+
+        this.time.delayedCall(1000, this.removeIFrame, [], this);
+    }
+
+    removeIFrame(){
+        this.player.player.clearTint()
+        this.player.player.invulnerable = false;
     }
 }
