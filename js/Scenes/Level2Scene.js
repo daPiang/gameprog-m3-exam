8cab0de04cb3a1e60fab2a0121563f3c3b5b8106
naAlmanzor <neilalmanzor28@gmail.com>
2022-05-03 15:24:02 +0800

    added teleporters in level 2

--- js/Scenes/Level2Scene.js
+++ js/Scenes/Level2Scene.js
@@ -29,30 +29,79 @@ export class Level2Scene extends Phaser.Scene {
 
     create() {
         this.map = this.make.tilemap({key: 'tilemap-2'});
-        this.tileset = this.map.addTilesetImage('tileset', 'otherworld-tiles');
+        this.tileset_1 = this.map.addTilesetImage('tileset', 'otherworld-tiles');
+        this.tileset_2 = this.map.addTilesetImage('portal', 'teleporter-tiles');
 
         // this.sky_bg = this.add.image(500, 400, 'sky').setScale(2.75).setScrollFactor(0);
         this.createMultipleImages(this, 0, 100, 8, 'sky', 0.20)
         this.createMultipleImages(this, 1, 130, 8, 'cloud1', 0.25);
         this.createMultipleImages(this, -25, 140, 8, 'cloud2', 0.5);
 
-        this.invisible_wall = this.map.createLayer('invisible wall', this.tileset, 0, 10);
-        this.brick_platform = this.map.createLayer('brick platforms', this.tileset, 0, 10);
-        this.main_platform = this.map.createLayer('main platform', this.tileset, 0, 10);
-        this.main_platform_details_back = this.map.createLayer('main details back', this.tileset, 0, 10);
-        this.main_platform_details_front = this.map.createLayer('main details front', this.tileset, 0, 10);
+        // Teleporters
+        this.tp = [
+            this.map.createLayer('tp 1', this.tileset_2, 0, 10),
+            this.map.createLayer('tp 2', this.tileset_2, 0, 10),
+            this.map.createLayer('tp 3', this.tileset_2, 0, 10),
+            this.map.createLayer('tp 4', this.tileset_2, 0, 10),
+            this.map.createLayer('tp 5', this.tileset_2, 0, 10),
+            this.map.createLayer('tp 6', this.tileset_2, 0, 10),
+            this.map.createLayer('tp 7', this.tileset_2, 0, 10)
+        ]
 
+        // Teleporter images
+        this.add.image(2773,480, 'portal-red').setScale(1.5);
+        this.add.image(2274, 514, 'portal-purple').setScale(1.5);
+        this.add.image(612, 410, 'portal-green').setScale(1.5);
+        this.add.image(1634, 480, 'portal-orange').setScale(1.5);
+        
+        this.add.image(552, 510, 'portal-green').setScale(1.5).flipX = true;
+        this.add.image(2509, 416, 'portal-purple').setScale(1.5).flipX = true;
+        this.add.image(92,480, 'portal-red').setScale(1.5).flipX=true;
+        
+        // Map
+        this.invisible_wall = this.map.createLayer('invisible wall', this.tileset, 0, 10);
+        this.brick_platform = this.map.createLayer('brick platforms', this.tileset_1, 0, 10);
+        this.main_platform = this.map.createLayer('main platform', this.tileset_1, 0, 10);
+        this.main_platform_details_back = this.map.createLayer('main details back', this.tileset_1, 0, 10);
+        this.main_platform_details_front = this.map.createLayer('main details front', this.tileset_1, 0, 10);
+
+        // Collision exclusion
         this.collisionExclusion(this.brick_platform);
         this.collisionExclusion(this.invisible_wall);
         this.collisionExclusion(this.main_platform);
+        this.collisionExclusion(this.tp[0]);
+        this.collisionExclusion(this.tp[1]);
+        this.collisionExclusion(this.tp[2]);
+        this.collisionExclusion(this.tp[3]);
+        this.collisionExclusion(this.tp[4]);
+        this.collisionExclusion(this.tp[5]);
+        this.collisionExclusion(this.tp[6]);
 
-        this.player = new Player(100, 300, this.physics, this.anims, this.input);
+        // Player and Monster
+        this.player = new Player(this, 100, 300)
+        // this.player = new Player(this,1600, 500)
         this.player.player.invulnerable = false;
         this.player.setWorldCollider(false);
 
-        this.physics.add.collider(this.player.player, this.main_platform);
-        this.physics.add.collider(this.player.player, this.invisible_wall);
-        this.physics.add.collider(this.player.player, this.brick_platform);
+
+        // Colliders
+        this.normal_collisions = [
+            this.physics.add.collider(this.player.player, this.main_platform),
+            this.physics.add.collider(this.player.player, this.invisible_wall),
+            this.physics.add.collider(this.player.player, this.brick_platform)
+        ]
+        
+        this.teleporter_collisions = [
+            this.physics.add.collider(this.player.player, this.tp[0], this.teleportTo7, null, this),
+            this.physics.add.collider(this.player.player, this.tp[1], this.teleportTo3, null, this),
+            this.physics.add.collider(this.player.player, this.tp[2], this.teleportTo2, null, this),
+            this.physics.add.collider(this.player.player, this.tp[3], this.teleportToIsland, null, this),
+            this.physics.add.collider(this.player.player, this.tp[4], this.teleportTo6, null, this),
+            this.physics.add.collider(this.player.player, this.tp[5], this.teleportTo5, null, this),
+            this.physics.add.collider(this.player.player, this.tp[6], this.teleportTo1, null, this),
+        ]
+
+        // this.hidden_portal =
 
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         this.cameras.main.startFollow(this.player.player).setZoom(2.52);
@@ -63,4 +112,34 @@ export class Level2Scene extends Phaser.Scene {
     update(){
         this.player.update();
     }
+
+    //teleporters
+    teleportTo1(player, teleporter){
+        player.setPosition(2760, 470)
+    }
+    
+    teleportTo2(player, teleporter){
+        player.setPosition(2530, 430)
+    }
+
+    teleportTo3(player, teleporter){
+        player.setPosition(2260, 520)
+    }
+
+    teleportToIsland(player, teleporter){
+        player.setPosition(2000, 100)
+    }
+    
+    teleportTo5(player, teleporter){
+        player.setPosition(570, 510)
+    }
+
+    teleportTo6(player, teleporter){
+        player.setPosition(600, 410)
+    }
+
+    teleportTo7(player, teleporter){
+        player.setPosition(110, 480)
+    }
+
 }
--- js/Scenes/SplashScene.js
+++ js/Scenes/SplashScene.js
@@ -22,9 +22,11 @@ export class SplashScene extends Phaser.Scene {
         this.load.atlas('player_atlas', './assets/atlas/player_atlas.png', './assets/atlas/player_atlas.json');
         this.load.atlas('mon_atlas', './assets/atlas/mon_atlas.png', './assets/atlas/mon_atlas.json');
 
+        // Maps
         this.load.image('brick-tiles', './assets/maps/tileset-bricks.png');
         this.load.image('platforming-tiles', './assets/maps/tileset-floors.png');
         this.load.image('otherworld-tiles', './assets/maps/tileset-otherworld.png');
+        this.load.image('teleporter-tiles', './assets/maps/tileset-teleporters.png')
         
         // Level-1
         this.load.image('moon', './assets/images/PARALLAX BG/MOON-BG.png');
@@ -40,6 +42,10 @@ export class SplashScene extends Phaser.Scene {
         this.load.image('sky', './assets/images/PARALLAX BG/SKY-BG.png');
         this.load.image('cloud1', './assets/images/PARALLAX BG/CLOUD-1.png');
         this.load.image('cloud2', './assets/images/PARALLAX BG/CLOUD-2.png');
+        this.load.image('portal-red', './assets/images/portal-red.png');
+        this.load.image('portal-purple', './assets/images/portal-purple.png');
+        this.load.image('portal-orange', './assets/images/portal-orange.png');
+        this.load.image('portal-green', './assets/images/portal-green.png');
 
         this.load.tilemapTiledJSON('tilemap-2', './assets/maps/stage-2.tmj');      
 
@@ -155,6 +161,6 @@ export class SplashScene extends Phaser.Scene {
         // });
 
         this.scene.start(SCENE_KEYS.SCENES.LEVEL_2);
-        this.scene.start(SCENE_KEYS.SCENES.LEVEL_1);
+        // this.scene.start(SCENE_KEYS.SCENES.LEVEL_1);
     }
 }
